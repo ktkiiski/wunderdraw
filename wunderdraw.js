f990953008fdbb9d144f97c30499a06256eee066
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2016-12-20 20:37:06 +0200

    Code re-factoring

--- wunderdraw.js
+++ wunderdraw.js
@@ -9,6 +9,7 @@ const COLOR_TURN_LEFT = [123, 131, 154, 255];
 const VECTOR_UP = [0, -1];
 const VECTOR_LEFT = [-1, 0];
 
+// Start drawing once the window has loaded the required image
 window.onload = () => {
     // Draw the source image to the canvas
     const sourceCanvas = document.createElement('canvas');
@@ -20,13 +21,14 @@ window.onload = () => {
 
     const targetCanvas = document.getElementById('target-canvas');
     const targetContext = targetCanvas.getContext('2d');
-    // Iterate through all the pixels. Start drawing from starting colors
+    // Iterate through all the pixels.
+    // Start drawing from the pixels with a starting color
     eachPixel(sourceContext, (color, x, y) => {
-        if (rgbEqual(color, COLOR_START_UP)) {
+        if (isEqualRGB(color, COLOR_START_UP)) {
             // Start drawing up
             draw(sourceContext, targetContext, x, y, VECTOR_UP);
         }
-        else if (rgbEqual(color, COLOR_START_LEFT)) {
+        else if (isEqualRGB(color, COLOR_START_LEFT)) {
             // Start drawing left
             draw(sourceContext, targetContext, x, y, VECTOR_LEFT);
         }
@@ -34,31 +36,32 @@ window.onload = () => {
 };
 
 /**
- * Draws the figure starting at the position
- * and towards the given direction.
+ * Draws a partial solution from the starting position towards the direction.
  */
-function draw(source, target, startX, startY, direction) {
+function draw(source, target, x, y, direction) {
+    // Draw pixel at the starting position
+    drawPixel(target, x, y);
+    // Advance to the next position
     const [dX, dY] = direction;
-    const endX = startX + dX;
-    const endY = startY + dY;
-    target.moveTo(startX, startY);
-    target.lineTo(endX, endY);
-    const color = getPixelColor(source, endX, endY);
-    if (rgbEqual(color, COLOR_STOP)) {
+    const nextX = x + dX;
+    const nextY = y + dY;
+    // Determine the next action from the color at the source image
+    const color = getPixelColor(source, nextX, nextY);
+    if (isEqualRGB(color, COLOR_STOP)) {
         // Stop drawing and finish the stroke
-        target.stroke();
+        drawPixel(target, nextX, nextY);
     }
-    else if (rgbEqual(color, COLOR_TURN_LEFT)) {
+    else if (isEqualRGB(color, COLOR_TURN_LEFT)) {
         // Turn left and continue drawing
-        draw(source, target, endX, endY, rotateLeft(direction));
+        draw(source, target, nextX, nextY, rotateLeft(direction));
     }
-    else if (rgbEqual(color, COLOR_TURN_RIGHT)) {
+    else if (isEqualRGB(color, COLOR_TURN_RIGHT)) {
         // Turn right and continue drawing
-        draw(source, target, endX, endY, rotateRight(direction));
+        draw(source, target, nextX, nextY, rotateRight(direction));
     }
     else {
         // Continue to the same direction
-        draw(source, target, endX, endY, direction);
+        draw(source, target, nextX, nextY, direction);
     }
 }
 
@@ -85,22 +88,29 @@ function getPixelColor(context, x, y) {
  * Returns whether or not two arrays, indicating RGBA values,
  * represents the same color.
  */
-function rgbEqual(rgb1, rgb2) {
+function isEqualRGB(rgb1, rgb2) {
     return rgb1.every((value, index) => rgb2[index] === value);
 }
 
 /**
- * Rotates the given direction vector by 90 degrees clockwise,
- * returning a copied vector.
+ * Rotates a direction vector 90 degrees clockwise.
  */
 function rotateRight([x, y]) {
     return [-y, x]; 
 }
 
 /**
- * Rotates the given direction vector by 90 degrees clockwise,
- * returning a copied vector.
+ * Rotates a direction vector 90 degrees counter-clockwise.
  */
 function rotateLeft([x, y]) {
     return [y, -x]; 
+}
+
+/**
+ * Draw a single black pixel to the canvas context.
+ */
+function drawPixel(context, x, y) {
+    const color = new Uint8ClampedArray([0, 0, 0, 255])
+    const pixel = new ImageData(color, 1, 1);
+    context.putImageData(pixel, x, y);
 }
